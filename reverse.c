89bf4dadaec254ed49a98cb10f892459c759c362
Md. Adib Zawad <158603527+adib-zawad@users.noreply.github.com>
2024-02-15 20:57:54 +0600

    Create Reverse.c

---
+++ reverse.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * TODO:
+ * In this file, you will write two functions:
+ *
+ * 1. reverse_arr: will take in two inputs a char* array and the number of
+ * elements in the array. It's responsible for reversing the input array. You
+ * should be modifying this array in place. Meaning that you shouldn't returning
+ * anything from this function.
+ *
+ * 2. swap: will take in two elements from the array and swap them. Returns
+ * nothing.
+ *
+ * Example:
+ * Given an array ["hello", "my", "name", "is"], after calling reverse_arr
+ * will result in ["is", "name", "my", "hello"]
+ */
+
+/* Must declare function at top
+ */
+
+void swap(char** a, char** b);
+
+void swap(char** a, char** b) {
+  char* temp = *a;
+  *a = *b;
+  *b = temp;
+}
+
+void reverse_arr(char** arr, int num) {
+  for (int i = 0; i < num / 2; i++) swap(&arr[i], &arr[num - 1 - i]);
+}
